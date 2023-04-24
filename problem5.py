cd9aa15e7567be77d094760b17bffffd83a0bdc5
FreekCool <freekcool2000@gmail.com>
2023-04-24 14:19:37 +0200

    Fixed problem 5

--- problem5.py
+++ problem5.py
@@ -35,12 +35,7 @@ counts = np.zeros((len(knowns), len(word_index_dict))) #Need to match the vocabu
 #TODO: iterate through file and update counts
 for line in f:
 
-    previous_word = '<s>'
-    previous_word_2 = '<s>'
-
     words = line.lower().split()       #Was first line.strip().split()
-
-    print(words)
 
     for i in range(2, len(words)):
         word = words[i]
@@ -49,7 +44,112 @@ for line in f:
 
         prev_word_2 = words[i - 2]
 
-        print(prev_word_2,prev_word,word)
+        if (prev_word_2, prev_word) in knowns:
+            knowns_index = knowns.index((prev_word_2, prev_word))
+
+            word_index = word_index_dict[word]
+
+            counts[knowns_index,word_index] += 1
+
+#TODO: normalize counts
+probs = normalize(counts, norm='l1', axis=1)
+
+'''Write unsmoothened probabilities to txt file'''
+with open('problem5_unsmooth.txt', 'w') as wf:
+    # For p(past | in, the)
+    prev_index = knowns.index(('in','the'))
+    word_index = word_index_dict['past']
+
+    prob = probs[prev_index,word_index]
+    wf.write(f'p(past | in, the) = {prob}\n')
+
+    # For p(time | in, the)
+    prev_index = knowns.index(('in','the'))
+    word_index = word_index_dict['time']
+
+    prob = probs[prev_index, word_index]
+    wf.write(f'p(time | in, the) = {prob}\n')
+
+    # For p(said | the, jury)
+    prev_index = knowns.index(('the', 'jury'))
+    word_index = word_index_dict['said']
+
+    prob = probs[prev_index, word_index]
+    wf.write(f'p(said | the, jury) = {prob}\n')
+
+    # For p(recommended | the, jury)
+    prev_index = knowns.index(('the', 'jury'))
+    word_index = word_index_dict['recommended']
+
+    prob = probs[prev_index, word_index]
+    wf.write(f'p(recommended | the, jury) = {prob}\n')
+
+    # For p(that | jury, said)
+    prev_index = knowns.index(('jury', 'said'))
+    word_index = word_index_dict['that']
+
+    prob = probs[prev_index, word_index]
+    wf.write(f'p(that | jury, said) = {prob}\n')
+
+    # For p(, | agriculture, teacher)
+    prev_index = knowns.index(('agriculture', 'teacher'))
+    word_index = word_index_dict[',']
+
+    prob = probs[prev_index, word_index]
+    wf.write(f'p(, | agriculture, teacher) = {prob}\n')
+
+'''Create the smoothened probabilities'''
+counts += 0.1
+
+#TODO: normalize counts
+probs = normalize(counts, norm='l1', axis=1)
+
+'''Write smoothened probabilities to txt file'''
+with open('problem5_smooth.txt', 'w') as wf:
+    # For p(past | in, the)
+    prev_index = knowns.index(('in','the'))
+    word_index = word_index_dict['past']
+
+    prob = probs[prev_index,word_index]
+    wf.write(f'p(past | in, the) = {prob}\n')
+
+    # For p(time | in, the)
+    prev_index = knowns.index(('in','the'))
+    word_index = word_index_dict['time']
+
+    prob = probs[prev_index, word_index]
+    wf.write(f'p(time | in, the) = {prob}\n')
+
+    # For p(said | the, jury)
+    prev_index = knowns.index(('the', 'jury'))
+    word_index = word_index_dict['said']
+
+    prob = probs[prev_index, word_index]
+    wf.write(f'p(said | the, jury) = {prob}\n')
+
+    # For p(recommended | the, jury)
+    prev_index = knowns.index(('the', 'jury'))
+    word_index = word_index_dict['recommended']
+
+    prob = probs[prev_index, word_index]
+    wf.write(f'p(recommended | the, jury) = {prob}\n')
+
+    # For p(that | jury, said)
+    prev_index = knowns.index(('jury', 'said'))
+    word_index = word_index_dict['that']
+
+    prob = probs[prev_index, word_index]
+    wf.write(f'p(that | jury, said) = {prob}\n')
+
+    # For p(, | agriculture, teacher)
+    prev_index = knowns.index(('agriculture', 'teacher'))
+    word_index = word_index_dict[',']
+
+    prob = probs[prev_index, word_index]
+    wf.write(f'p(, | agriculture, teacher) = {prob}\n')
+
+
+
 
 
 
